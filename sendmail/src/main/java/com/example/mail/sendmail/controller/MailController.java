a9f610de57dff34f64de105fa04e2cb6776eccce
geeklh <1033797938@qq.com>
2019-11-20 14:24:05 +0800

    邮箱

--- sendmail/src/main/java/com/example/mail/sendmail/controller/MailController.java
+++ sendmail/src/main/java/com/example/mail/sendmail/controller/MailController.java
@@ -5,6 +5,7 @@ import com.example.mail.sendmail.service.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,13 +22,14 @@ public class MailController {
 
 
     @RequestMapping(value = "/sendMail")
+    @ResponseBody
     public String sendEmail() {
         Email email_email = new Email();
         List<String> addressList = new ArrayList<String>();
-        addressList.add("xxx@163.com");
+        addressList.add("1033797938@qq.com");
         email_email.setToAddress(addressList);
         email_email.setSubject("java碎碎念-主题测试");// 主题
-        email_email.setContent("你好！<br><br> 测试邮件发送成功！");
+        email_email.setContent("你好！<br><br> 测试邮件发送成功！");//内容
         // 发送邮件
         mailService.sendMail(email_email);
 
